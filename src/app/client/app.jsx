57e5b77cc0c16cd6505dbd03d7878d4a4a095235
Jorge Ivan Morales <jorge.iv.morales@gmail.com>
2016-07-26 21:27:36 -0500

    Finished app

--- src/app/client/app.jsx
+++ src/app/client/app.jsx
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        < Header/ >
+        <Header />
         {this.props.children}
       </div>
     )
--- src/app/client/bins/bins_main.jsx
+++ src/app/client/bins/bins_main.jsx
@@ -3,6 +3,7 @@ import { createContainer } from 'meteor/react-meteor-data';
 import { Bins } from 'app/imports/collections/bins';
 import BinsEditor from './bins_editor';
 import BinsViewer from './bins_viewer';
+import BinsShare from './bins_share';
 
 class BinsMain extends Component {
   render() {
@@ -14,13 +15,16 @@ class BinsMain extends Component {
       <div>
         <BinsEditor bin={this.props.bin} />
         <BinsViewer bin={this.props.bin} />
+        <BinsShare bin={this.props.bin} />
       </div>
     )
   }
 }
 
 export default createContainer((props) => {
-  Meteor.subscribe('bins');
   const { binId } = props.params;
+  Meteor.subscribe('bins');
+  Meteor.subscribe('sharedBins');
+
   return { bin: Bins.findOne(binId) }
 }, BinsMain);
